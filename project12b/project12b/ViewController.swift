1f9b2fd68f04ab6a40a093efeae4eb91ba045e88
Sergey Plotnikov <sergpl.home@gmail.com>
2020-08-04 13:50:06 +0200

    store in JSON through Codable protocol,

--- project12b/project12b/ViewController.swift
+++ project12b/project12b/ViewController.swift
@@ -18,12 +18,34 @@ class ViewController: UICollectionViewController,UIImagePickerControllerDelegate
         super.viewDidLoad()
         // Do any additional setup after loading the view.
        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
-    }
+        
+        let defaults = UserDefaults.standard
 
+if let savedPeople = defaults.object(forKey: "people") as? Data {
+        let jsonDecoder = JSONDecoder()
+            do {
+                people = try jsonDecoder.decode([Person].self, from: savedPeople)
+            } catch {
+                print("Failed to load people")
+            }
+        }
+        
+        
+    }
 
   //  override func numberOfSections(in collectionView: UICollectionView) -> Int {
  //   return 1
   //  }
+    
+    func save() {
+    let jsonEncoder = JSONEncoder()
+    if let savedData = try? jsonEncoder.encode(people) {
+            let defaults = UserDefaults.standard
+            defaults.set(savedData, forKey: "people")
+        } else {
+        print("Failed to save people.")
+        }
+    }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         //print(section)
@@ -74,6 +96,7 @@ class ViewController: UICollectionViewController,UIImagePickerControllerDelegate
              guard let newName = ac2?.textFields?[0].text else { return }
              person.name = newName
              self?.collectionView.reloadData()
+            self?.save()
          })
         self?.present(ac2, animated: true)
     })
@@ -86,6 +109,7 @@ class ViewController: UICollectionViewController,UIImagePickerControllerDelegate
         })
          ac2.addAction(UIAlertAction(title: "No", style: .cancel))
         self?.present(ac2, animated: true)
+        self?.save()
            })
     present(ac, animated: true)
 }
